3fb67a74069aeacba8809e19609c338f49bd9652
dolu1990 <papuchoud@hotmail.com>
2014-05-27 00:13:43 +0200

    tortoise do ..., sometime

--- mods/eln/client/SoundLoader.java
+++ mods/eln/client/SoundLoader.java
@@ -1,25 +1,43 @@
 package mods.eln.client;
 
+import java.util.HashMap;
+
 import mods.eln.Eln;
 import net.minecraftforge.client.event.sound.SoundLoadEvent;
 import net.minecraftforge.event.ForgeSubscribe;
 
 public class SoundLoader {
-	
-    @ForgeSubscribe
-    public void onSound(SoundLoadEvent event) {
-        try {
-            //event.manager.soundPoolSounds.addSound("eln/sound/alarma.ogg", Eln.class.getResource("sound/alarma.ogg"));            
-            //event.manager.soundPoolSounds.addSound("eln/sound/smallalarm_critical.ogg", Eln.class.getResource("sound/smallalarm_critical.ogg"));            
-            event.manager.soundPoolSounds.addSound("eln:alarma.ogg");            
-            event.manager.soundPoolSounds.addSound("eln:smallalarm_critical.ogg");  
-            event.manager.soundPoolSounds.addSound("eln:wind_turbine.ogg");
-            event.manager.soundPoolSounds.addSound("eln:water_turbine.ogg");
-            event.manager.soundPoolSounds.addSound("eln:heat_turbine_200v.ogg");
-            event.manager.soundPoolSounds.addSound("eln:heat_turbine_50v.ogg");
-        } 
-        catch (Exception e) {
-            System.err.println("Failed to register one or more sounds.");
-        }
-    }
+
+	@ForgeSubscribe
+	public void onSound(SoundLoadEvent event) {
+		try {
+
+			event.manager.soundPoolSounds.addSound("eln:alarma.ogg");
+			event.manager.soundPoolSounds.addSound("eln:smallalarm_critical.ogg");
+			event.manager.soundPoolSounds.addSound("eln:wind_turbine.ogg");
+			event.manager.soundPoolSounds.addSound("eln:water_turbine.ogg");
+			event.manager.soundPoolSounds.addSound("eln:heat_turbine_200v.ogg");
+			//event.manager.soundPoolSounds.addSound("eln:heat_turbine_50v.ogg");
+			//event.manager.soundPoolSounds.addSound("eln:heat_turbine_50v_0x.ogg");
+			loadMultiTrack(event, "eln:heat_turbine_50v", 3);
+		} catch (Exception e) {
+			System.err.println("Failed to register one or more sounds.");
+		}
+	}
+
+	public void loadMultiTrack(SoundLoadEvent event, String name, int count) {
+		for (int idx = 0; idx < count; idx++) {
+			event.manager.soundPoolSounds.addSound(name + "_" + idx + "x.ogg");
+		}
+		multiTrackMap.put(name, count);
+	}
+
+	static HashMap<String, Integer> multiTrackMap = new HashMap<String, Integer>();
+
+	static public int getTrackCount(String name) {
+		Integer i = multiTrackMap.get(name);
+		if (i == null)
+			return 1;
+		return i;
+	}
 }
--- mods/eln/sound/SoundClient.java
+++ mods/eln/sound/SoundClient.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 
 import javax.swing.text.html.parser.Entity;
 
+import mods.eln.client.SoundLoader;
 import mods.eln.misc.Utils;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.SoundPool;
+import net.minecraft.client.audio.SoundPoolEntry;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
@@ -21,15 +24,34 @@ public class SoundClient {
 		if(world.provider.dimensionId != player.dimension) return;
 		double distance = Math.sqrt(Math.pow(x-player.posX, 2) + Math.pow(y-player.posY, 2) + Math.pow(z-player.posZ, 2));
 		if(distance >= rangeMax) return;
+		float distanceFactor = 1;
 		if(distance > rangeNominal){
-			volume *= (rangeMax - distance)/(rangeMax-rangeNominal);
+			distanceFactor = (float) ((rangeMax - distance)/(rangeMax-rangeNominal));
 		}
 		
-		float b = Utils.traceRay(world, player.posX, player.posY, player.posZ, x, y, z,new Utils.TraceRayWeightOpaque());
-		volume -= b*0.2;
-		//System.out.println(b);
+		float blockFactor = Utils.traceRay(world, player.posX, player.posY, player.posZ, x, y, z,new Utils.TraceRayWeightOpaque());
+
+		int trackCount = SoundLoader.getTrackCount(track);
 		
-		if(volume <= 0) return;
-		world.playSound(player.posX + (x-player.posX)/distance, player.posY + (y-player.posY)/distance, player.posZ + (z-player.posZ)/distance, track, volume, pitch, false);
+		if(trackCount == 1){
+			volume -= blockFactor*0.2f;
+			volume *= distanceFactor;
+			if(volume <= 0) return;
+			world.playSound(player.posX + (x-player.posX)/distance, player.posY + (y-player.posY)/distance, player.posZ + (z-player.posZ)/distance, track, volume, pitch, false);
+		}else{
+			for(int idx = 0;idx < trackCount;idx++){
+				float bandVolume = volume;
+				bandVolume *= distanceFactor;
+				float normalizedBlockFactor = blockFactor*0.5f;
+				
+				bandVolume -= ((trackCount-1-idx)/(trackCount-1f)+0.2)*normalizedBlockFactor;
+				Utils.print(bandVolume + " ");
+				world.playSound(player.posX + (x-player.posX)/distance, player.posY + (y-player.posY)/distance, player.posZ + (z-player.posZ)/distance, track + "_" + idx + "x", bandVolume, pitch, false);
+			}
+			Utils.println("");
+		}
+		
+		
+		
 	}
 }
