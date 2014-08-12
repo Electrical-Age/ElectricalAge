58f91043a7d74e99f432ca0992eeb37791ab6d7a
ShadowWarrior979 <jackpitman99@hotmail.co.uk>
2014-08-12 19:04:50 +0100

    keep

---
+++ mods/eln/Book.java
@@ -0,0 +1,42 @@
+package mods.eln;
+
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+
+
+
+
+	public class Book {
+	public void processCommand(ICommandSender sender, String[] data) {
+		       
+		       
+		       EntityPlayer player = (EntityPlayer) sender;
+		       
+		
+		       ItemStack Book = new ItemStack(Items.written_book);
+		      
+		       
+		       NBTTagCompound tag = new NBTTagCompound();
+		       NBTTagList bookPages = new NBTTagList();
+		       bookPages.appendTag(new NBTTagString("Test"));
+		       bookPages.appendTag(new NBTTagString("Test"));
+		       bookPages.appendTag(new NBTTagString("Test"));
+		       bookPages.appendTag(new NBTTagString("Test"));
+		       bookPages.appendTag(new NBTTagString("Test"));
+		       bookPages.appendTag(new NBTTagString("Test"));
+		       bookPages.appendTag(new NBTTagString("Test"));
+		       Book.setTagInfo("pages", bookPages);
+		       Book.setTagInfo("author", new NBTTagString("ShadowWarrior979"));
+		       Book.setTagInfo("title", new NBTTagString("Test"));
+
+		       
+		       player.inventory.addItemStackToInventory(Book);
+	}
+	}
