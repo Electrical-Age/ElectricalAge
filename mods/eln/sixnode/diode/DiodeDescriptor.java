dfc77e63f3f9ddea35eaef394bab898a3b5b7796
cm0x4D <cm0x4d@codemonkey.ch>
2014-10-01 22:45:23 +0200

    Diode renders 3D in inventory.

--- mods/eln/sixnode/diode/DiodeDescriptor.java
+++ mods/eln/sixnode/diode/DiodeDescriptor.java
@@ -58,7 +58,28 @@ public class DiodeDescriptor extends SixNodeDescriptor {
 		super.setParent(item, damage);
 		Data.addEnergy(newItemStack());
 	}
-	
+
+    @Override
+    public boolean use2DIcon() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item,
+                                         ItemRendererHelper helper) {
+        return true;
+    }
+
+    @Override
+    public boolean handleRenderType(ItemStack item, ItemRenderType type) {
+        return true;
+    }
+
+    @Override
+    public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
+        draw();
+    }
+
 /*	public void applyTo(DiodeProcess diode) {
 		diode.IfU = IfU;
 	}
