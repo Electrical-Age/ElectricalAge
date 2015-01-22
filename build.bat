50dc394ac7bfe79802cc592a63fa960642339ede
dolu1990 <papuchoud@hotmail.com>
2015-01-22 21:52:37 +0100

    move directories, add gradle script libs contain some jar

---
+++ build.bat
@@ -0,0 +1,9 @@
+del %appdata%\.minecraft\mods\ElectricalAge*
+
+gradlew build
+
+copy build\libs\ElectricalAge_BETA-1.0.jar %appdata%\.minecraft\mods\ElectricalAge_BETA-1.x_rxx.jar
+
+echo END OF SCRIPT
+pause
+pause
---
+++ push.bat
@@ -0,0 +1,5 @@
+del %appdata%\.minecraft\mods\ElectricalAge*
+
+copy build\libs\ElectricalAge_BETA-1.0.jar %appdata%\.minecraft\mods\ElectricalAge_BETA-1.x_rxx.jar
+
+pause
