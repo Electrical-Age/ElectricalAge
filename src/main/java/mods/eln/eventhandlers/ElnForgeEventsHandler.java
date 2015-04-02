a4054b28079dc8c325904ae60133d6ae67e750c7
Gregory Maddra <bloxgate@gmail.com>
2015-04-01 19:52:39 -0500

    Fixes server side crashes

--- src/main/java/mods/eln/eventhandlers/ElnForgeEventsHandler.java
+++ src/main/java/mods/eln/eventhandlers/ElnForgeEventsHandler.java
@@ -1,6 +1,8 @@
 package mods.eln.eventhandlers;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import mods.eln.Eln;
 import mods.eln.achievepackets.AchievePacket;
 import mods.eln.wiki.Root;
@@ -9,6 +11,7 @@ import net.minecraftforge.client.event.GuiOpenEvent;
 public class ElnForgeEventsHandler {
 
     @SubscribeEvent
+    @SideOnly(Side.CLIENT)
     public void openGuide(GuiOpenEvent e) {
         if (e.gui instanceof Root) {
             Eln.achNetwork.sendToServer(new AchievePacket("openWiki"));
