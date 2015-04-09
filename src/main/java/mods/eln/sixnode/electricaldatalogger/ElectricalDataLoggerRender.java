97e856a1f11ca828333d2cd119a872be2be8dc4b
cm0x4D <cm0x4d@codemonkey.ch>
2015-04-09 23:41:42 +0200

    Fixes bug where signal input pin is rendered on non-industrial loggers.

--- src/main/java/mods/eln/sixnode/electricaldatalogger/ElectricalDataLoggerRender.java
+++ src/main/java/mods/eln/sixnode/electricaldatalogger/ElectricalDataLoggerRender.java
@@ -40,7 +40,9 @@ public class ElectricalDataLoggerRender extends SixNodeElementRender {
 	@Override
 	public void draw() {
 		super.draw();
-        drawSignalPin(front.inverse(), new float[] { 6.37f, 6.37f, 5.67f, 6.12f });
+        if (!descriptor.onFloor) {
+            drawSignalPin(front.inverse(), new float[]{6.37f, 6.37f, 5.67f, 6.12f});
+        }
         descriptor.draw(log, front);
 	}
 
