3870ed0260a81493909b8c9fc34d85a87bb9f668
cm0x4D <cm0x4d@codemonkey.ch>
2015-12-30 22:56:03 +0100

    tr function supports now arguments.
    The position of the arguments inside the string can be controlled using %{index} placeholders. This allows the parameters to have different positions for different languages.
    Only %1 to %9 (Argument at index 0 to 8) are supported, no escaping to print %1 and so on possible yet.

--- src/main/java/mods/eln/misc/I18N.java
+++ src/main/java/mods/eln/misc/I18N.java
@@ -13,16 +13,30 @@ public class I18N {
 		return FMLCommonHandler.instance().getCurrentLanguage();
 	}
 
-	public static String tr(String key) {
-		return tr(key, getCurrentLanguage());
+	/*
+	TODO: Only argument placeholders from %1 to %9 (9 arguments) are supported.
+	TODO: There is no escaping of the strings %1 and so on possible.
+	 */
+	public static String tr(String text, Object... objects) {
+		String translation = l.getStringLocalization(text, getCurrentLanguage());
+		if (translation == null || "".equals(translation)) {
+			translation = text;
+		}
+
+		for (int i = 0; i < objects.length; ++i) {
+			String stringRepresentation = objects[i] != null ? objects[i].toString() : "null";
+			translation = translation.replace("%" + (i + 1), stringRepresentation);
+		}
+
+		return translation;
 	}
 
-	public static String tr(String key, String lang) {
-		String translation = l.getStringLocalization(key, lang);
+	public static String translateToLanguage(String text, String language) {
+		String translation = l.getStringLocalization(text, language);
 		if (translation != null && !"".equals(translation)) {
 			return translation;
 		} else {
-			return key;
+			return text;
 		}
 	}
 }
