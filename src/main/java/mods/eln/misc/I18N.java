7662312af243157cb24206baabde1829ad26528e
cm0x4D <cm0x4d@codemonkey.ch>
2016-01-01 16:02:30 +0100

    Strings appearing in multiple files are only present once in language file

--- src/main/java/mods/eln/misc/I18N.java
+++ src/main/java/mods/eln/misc/I18N.java
@@ -12,128 +12,157 @@ import java.util.regex.Pattern;
  * Internationalization and localization helper class.
  */
 public class I18N {
-	private final static LanguageRegistry languageRegistry = LanguageRegistry.instance();
+    private final static LanguageRegistry languageRegistry = LanguageRegistry.instance();
 
-	public static String getCurrentLanguage() {
-		return FMLCommonHandler.instance().getCurrentLanguage();
-	}
+    public static String getCurrentLanguage() {
+        return FMLCommonHandler.instance().getCurrentLanguage();
+    }
 
     /**
      * Translates the given string. You can actually pass arguments to the method and reference them in the string using
      * the placeholders %1 up to %9. For simplicity only up to 9 placeholders are allowed. Newlines are not supported
      * at all, so if you have to translate multiline strings, use tr() for each line. The lines will follow one after
      * another in the language anyway.
-     *
+     * <p/>
      * Example: tr("You have %1 lives left", 4);
-     *
+     * <p/>
      * IT IS IMPORTANT THAT YOU PASS THE STRING LITERALS AT LEAST ONCE AS THE FIRST PARAMETER TO THIS METHOD!!
      * Otherwise the translation will not be added to the language files automatically. There is no problem to use the
      * tr() method afterwards using an already registered string in the code using a string variable again.
-     *
+     * <p/>
      * TODO: There is no escaping of the strings %1 to %9 possible.
      *
-     * @param text      Text to translate
-     * @param objects   0 up to 9 arguments to integrate into the text.
-     * @return          Translated text or original text if no translation is present.
+     * @param text    Text to translate
+     * @param objects 0 up to 9 arguments to integrate into the text.
+     * @return Translated text or original text if no translation is present.
      */
-	public static String tr(final String text, Object... objects) {
-		// Assert correct number of objects and no newline in text.
-		assert(objects.length <= 9);
-		assert(!text.contains("\n"));
+    public static String tr(final String text, Object... objects) {
+        // Assert correct number of objects and no newline in text.
+        assert (objects.length <= 9);
+        assert (!text.contains("\n"));
 
-		// Try to find the translation for the string using forge API.
-		String translation = languageRegistry.getStringLocalization(text);
+        // Try to find the translation for the string using forge API.
+        String translation = languageRegistry.getStringLocalization(text);
 
-		// If no translation was found, just use the original text.
-		if (translation == null || "".equals(translation)) {
-			translation = text;
+        // If no translation was found, just use the original text.
+        if (translation == null || "".equals(translation)) {
+            translation = text;
 
             // TODO: Maybe we could save the information about the missing translation or make a HTTP post...
-		}
+        }
 
-		// Replace placeholders in string by actual string values of the passed objects.
-		for (int i = 0; i < objects.length; ++i) {
-			translation = translation.replace("%" + (i + 1), String.valueOf(objects[i]));
-		}
+        // Replace placeholders in string by actual string values of the passed objects.
+        for (int i = 0; i < objects.length; ++i) {
+            translation = translation.replace("%" + (i + 1), String.valueOf(objects[i]));
+        }
 
-		return translation;
-	}
+        return translation;
+    }
 
-	private static class SourceCodeParser {
-		private static final Pattern JAVA_PATTERN = Pattern.compile("tr\\s*\\(\\s*\"(.*?)\"\\s*[,)]");
+    private static class SourceCodeParser {
+        private static final Pattern JAVA_PATTERN = Pattern.compile("tr\\s*\\(\\s*\"(.*?)\"\\s*[,)]");
+        private static final String MULTIPLE_LOCATIONS = "Appears in multiple source files";
 
-		public static void parseSourceFolderRecursive(final File folder, final Map<String, Set<String>> strings)
+        public static Map<String, Set<String>> parseSourceFolder(final File file) throws IOException {
+            Map<String, Set<String>> strings = new TreeMap<String,Set<String>>();
+            strings.put(MULTIPLE_LOCATIONS, new TreeSet<String>());
+            parseSourceFolderRecursive(file, strings);
+            return strings;
+        }
+
+        private static void parseSourceFolderRecursive(final File folder, final Map<String, Set<String>> strings)
             throws IOException {
-			// Check that arguments are valid.
-			if (folder != null && folder.exists() &&  strings != null) {
+            // Check that arguments are valid.
+            if (folder != null && folder.exists() && strings != null) {
 
-				// Do for each file.
-				for (final File file: folder.listFiles()) {
+                // Do for each file.
+                for (final File file : folder.listFiles()) {
 
-					// If the file is a directory, call the method recursively.
-					if (file.isDirectory()) {
-						parseSourceFolderRecursive(file, strings);
+                    // If the file is a directory, call the method recursively.
+                    if (file.isDirectory()) {
+                        parseSourceFolderRecursive(file, strings);
 
-					// If it is a file and has the file extension .java, parse the Java file.
-					} else if (file.isFile() && file.getName().endsWith(".java")) {
-						System.out.println("Parsing Java source file: " + file.getName() + "...");
-						parseJavaFile(file, strings);
+                        // If it is a file and has the file extension .java, parse the Java file.
+                    } else if (file.isFile() && file.getName().endsWith(".java")) {
+                        System.out.println("Parsing Java source file: " + file.getName() + "...");
+                        parseJavaFile(file, strings);
 
-					// If it is a file and has the file extension .kt, parse the Kotlin file.
-					} else if (file.isFile() && file.getName().endsWith(".kt")) {
-						System.out.println("Parsing Kotlin source file: " + file.getName() + "...");
-						parseKotlinFile(file, strings);
-					}
-				}
-			}
-		}
+                        // If it is a file and has the file extension .kt, parse the Kotlin file.
+                    } else if (file.isFile() && file.getName().endsWith(".kt")) {
+                        System.out.println("Parsing Kotlin source file: " + file.getName() + "...");
+                        parseKotlinFile(file, strings);
+                    }
+                }
+            }
+        }
 
-		private static void parseJavaFile(final File file, final Map<String,Set<String>> strings) throws IOException {
-			// Load file into memory.
-			final String content = new Scanner(file).useDelimiter("\\Z").next();
+        private static void parseJavaFile(final File file, final Map<String, Set<String>> strings) throws IOException {
+            // Load file into memory.
+            final String content = new Scanner(file).useDelimiter("\\Z").next();
 
-			// Find all matches for Java style translations.
-			final Set<String> translations = new TreeSet<String>();
-			final Matcher matcher = JAVA_PATTERN.matcher(content);
-			while (matcher.find()) {
-				System.out.println(matcher.group(1));
-				translations.add(matcher.group(1));
-			}
+            // Find all matches for Java style translations.
+            final Set<String> translations = new TreeSet<String>();
+            final Matcher matcher = JAVA_PATTERN.matcher(content);
+            while (matcher.find()) {
+                String translation = matcher.group(1);
+                System.out.println("  " + translation);
 
-			// If there were translations for that file, add the list of translations to the map.
-			if (!translations.isEmpty()) {
-				strings.put(file.getPath(), translations);
-			}
-		}
+                if (!isStringAlreadyPresent(translation, strings)) {
+                    translations.add(translation);
+                }
+            }
 
-		private static void parseKotlinFile(final File file, final Map<String, Set<String>> strings) {
-			throw new UnsupportedOperationException();
-		}
-	}
+            // If there were translations for that file, add the list of translations to the map.
+            if (!translations.isEmpty()) {
+                strings.put(file.getPath(), translations);
+            }
+        }
 
-	private static class LanguageFileGenerator {
+        private static void parseKotlinFile(final File file, final Map<String, Set<String>> strings) {
+            throw new UnsupportedOperationException();
+        }
+
+        private static boolean isStringAlreadyPresent(final String string,
+                                                      final Map<String, Set<String>> strings) {
+            for (final String fileName: strings.keySet()) {
+                if (MULTIPLE_LOCATIONS.equals(fileName)) {
+                    if (strings.get(fileName).contains(string))
+                        return true;
+                } else {
+                    if (strings.get(fileName).remove(string)) {
+                        strings.get(MULTIPLE_LOCATIONS).add(string);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+
+    private static class LanguageFileGenerator {
         private static final String FILE_HEADER = "#<ELN_LANGFILE_V1_1>\n";
 
-		public static void generateNewFile(final File file, final Map<String, Set<String>> strings)
+        public static void generateNewFile(final File file, final Map<String, Set<String>> strings)
             throws IOException {
-			final FileWriter writer = new FileWriter(file);
+            final FileWriter writer = new FileWriter(file);
 
             // Write header.
             writer.append(FILE_HEADER);
 
             // For each source file with translations create the file comment.
-			for (final String sourceFile: strings.keySet()) {
-				writer.append("\n# ").append(sourceFile).append("\n");
+            for (final String sourceFile : strings.keySet()) {
+                writer.append("\n# ").append(sourceFile).append("\n");
 
                 // For each translated string in that source file, add translation text.
-				for (final String translation: strings.get(sourceFile)) {
-					writer.append(translation).append("=\n");
-				}
-			}
+                for (final String translation : strings.get(sourceFile)) {
+                    writer.append(translation).append("=\n");
+                }
+            }
 
             // Close writer.
-			writer.close();
-		}
+            writer.close();
+        }
 
         public static void updateFile(final File file, final Map<String, Set<String>> strings,
                                       final Map<String, String> existingTranslations) throws IOException {
@@ -143,11 +172,11 @@ public class I18N {
             writer.append(FILE_HEADER);
 
             // For each source file with translations create the file comment.
-            for (final String sourceFile: strings.keySet()) {
+            for (final String sourceFile : strings.keySet()) {
                 writer.append("\n# ").append(sourceFile).append("\n");
 
                 // For each translated string in that source file, add translation text.
-                for (final String translation: strings.get(sourceFile)) {
+                for (final String translation : strings.get(sourceFile)) {
                     writer.append(translation).append("=");
 
                     final String existingTranslation = existingTranslations.get(translation);
@@ -167,8 +196,10 @@ public class I18N {
     private static class LanguageFileParser {
         private static final Pattern TRANSLATION_PATTERN = Pattern.compile("^([^=]+)=([^=]+)$");
 
-        public static Map<String,String> parseFile(final File file) throws IOException {
-            final Map<String,String> translations = new TreeMap<String, String>();
+        public static Map<String, String> parseFile(final File file) throws IOException {
+            final Map<String, String> translations = new TreeMap<String, String>();
+
+            // TODO: Check header.
 
             final BufferedReader reader = new BufferedReader(new FileReader(file));
             String line;
@@ -200,14 +231,13 @@ public class I18N {
                 usage();
 
             // If the language file already exists, parse the existing translations from the language file.
-            Map<String,String> existingTranslations = null;
+            Map<String, String> existingTranslations = null;
             if (languageFile.isFile()) {
                 existingTranslations = LanguageFileParser.parseFile(languageFile);
             }
 
             // Get the string to translate from the source code.
-            Map<String,Set<String>> stringsToTranslate = new TreeMap<String, Set<String>>();
-            SourceCodeParser.parseSourceFolderRecursive(srcFolder, stringsToTranslate);
+            Map<String, Set<String>> stringsToTranslate = SourceCodeParser.parseSourceFolder(srcFolder);
 
             // If there was no file present, just generate the language file, otherwise update the existing file.
             if (existingTranslations == null) {
@@ -215,9 +245,9 @@ public class I18N {
             } else {
                 LanguageFileGenerator.updateFile(languageFile, stringsToTranslate, existingTranslations);
             }
-		} catch (IOException e) {
-			e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
             System.exit(1);
-		}
-	}
+        }
+    }
 }
