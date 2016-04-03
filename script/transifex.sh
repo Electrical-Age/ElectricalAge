1f26c673d81593bb229a685215c1bb1dba57d9ec
Christopher Métrailler <christopher.metrailler@hevs.ch>
2016-04-03 19:03:46 +0200

    Use Travis to generate the latest source lang file and push it to Transifex. Chmod scripts to be exectutable.

--- script/transifex.sh
+++ script/transifex.sh
@@ -2,19 +2,24 @@
 
 # Script updated from git://github.com/tangentlabs/django-oscar.gitmaster
 #
-# Push source and translation files to Transifex.
+# Generate and push the source language file to Transifex.
 # This script is called after every successful build on Travis CI.
 # It relies on $TRANSIFEX_PASSWORD being set in .travis.yml
 
 if [ "$TRAVIS_REPO_SLUG" == "Electrical-Age/ElectricalAge" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "1.7.10-MNA" ]
 then
-    echo "Submitting translation files to Transifex"
-    pip install transifex-client
-    # Write .transifexrc file
-    echo "[https://www.transifex.com]
+
+  echo "Generating the latest language source file"
+  ./gradlew updateMasterLanguageFile
+
+  echo "Submitting the generated translation source file to Transifex"
+  pip install transifex-client
+  # Write .transifexrc file
+  echo "[https://www.transifex.com]
 hostname = https://www.transifex.com
 password = $TRANSIFEX_PASSWORD
 token =
 username = metc" > ~/.transifexrc
-    tx push --source --translations --no-interactive --skip
+  
+  tx push --source --translations -l en_us --no-interactive --skip
 fi
