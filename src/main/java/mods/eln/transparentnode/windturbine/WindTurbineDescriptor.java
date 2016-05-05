1f18c95558a7739375105863408a26ff43e06c20
cm0x4D <cm0x4d@codemonkey.ch>
2016-05-05 22:15:43 +0200

    Fixes #270, code cleanup

--- src/main/java/mods/eln/transparentnode/windturbine/WindTurbineDescriptor.java
+++ src/main/java/mods/eln/transparentnode/windturbine/WindTurbineDescriptor.java
@@ -16,145 +16,134 @@ import java.util.List;
 import static mods.eln.i18n.I18N.tr;
 
 public class WindTurbineDescriptor extends TransparentNodeDescriptor {
+    private final Obj3D obj;
+    private final Obj3DPart main;
+    private final Obj3DPart rot;
+    private final Obj3DPart halo;
 
+    public final ElectricalCableDescriptor cable;
 
+    final double nominalPower;
+    final double maxVoltage;
+    private final double maxWind;
+    final int offY;
+    final int rayX;
+    final int rayY;
+    final int rayZ;
+    int blockMalusSubCount;
+    final double blockMalus;
+    final String soundName;
+    final float nominalVolume;
+    final FunctionTable PfW;
+    double speed;
 
-	public WindTurbineDescriptor(
-			String name,Obj3D obj,
-			ElectricalCableDescriptor cable,
-			FunctionTable PfW,
-			double nominalPower,double nominalWind,
-			double maxVoltage, double maxWind,
-			int offY,
-			int rayX,int rayY,int rayZ,
-			int blockMalusMinCount,double blockMalus, 
-			String soundName, float nominalVolume
-			) {
-		super(name, WindTurbineElement.class, WindTurbineRender.class);
+    public WindTurbineDescriptor(String name, Obj3D obj, ElectricalCableDescriptor cable, FunctionTable PfW,
+                                 double nominalPower, double nominalWind, double maxVoltage, double maxWind, int offY,
+                                 int rayX, int rayY, int rayZ, int blockMalusMinCount, double blockMalus,
+                                 String soundName, float nominalVolume) {
+        super(name, WindTurbineElement.class, WindTurbineRender.class);
 
-		this.cable = cable;
-		this.nominalPower = nominalPower;
-		this.nominalWind = nominalWind;
-		this.maxVoltage = maxVoltage;
-		this.maxWind = maxWind;
-		this.offY = offY;
-		this.rayX = rayX;
-		this.rayY = rayY;
-		this.rayZ = rayZ;
-		this.blockMalusSubCount = blockMalusMinCount + 1;
-		this.blockMalus = blockMalus;
-		this.soundName = soundName;
-		this.nominalVolume = nominalVolume;
-		this.PfW = PfW.duplicate(nominalWind, nominalPower);
-		this.obj = obj;
-		if(obj != null){
-			main = obj.getPart("main");
-			rot = obj.getPart("rot");
-			halo = obj.getPart("halo");
-			if(rot != null){
-				speed = rot.getFloat("speed");
-			}
-		}
+        this.cable = cable;
+        this.nominalPower = nominalPower;
+        this.maxVoltage = maxVoltage;
+        this.maxWind = maxWind;
+        this.offY = offY;
+        this.rayX = rayX;
+        this.rayY = rayY;
+        this.rayZ = rayZ;
+        this.blockMalusSubCount = blockMalusMinCount + 1;
+        this.blockMalus = blockMalus;
+        this.soundName = soundName;
+        this.nominalVolume = nominalVolume;
+        this.PfW = PfW.duplicate(nominalWind, nominalPower);
+        this.obj = obj;
+        if (obj != null) {
+            main = obj.getPart("main");
+            rot = obj.getPart("rot");
+            halo = obj.getPart("halo");
+            if (rot != null) {
+                speed = rot.getFloat("speed");
+            }
+        } else {
+            main = null;
+            rot = null;
+            halo = null;
+        }
 
-		voltageLevelColor = VoltageLevelColor.LowVoltage;
-	}
-	
-	public void setParent(net.minecraft.item.Item item, int damage)
-	{
-		super.setParent(item, damage);
-		Data.addEnergy(newItemStack());
-	}
-	public double speed;
-	
-	public void setGhostGroup(GhostGroup ghostGroup)
-	{
-		blockMalusSubCount += ghostGroup.size();
-		this.ghostGroup = ghostGroup;
-	}
-	
-	
-	Obj3DPart main,rot,halo;
-	
-	Obj3D obj;
-	public ElectricalCableDescriptor cable;
-	public double nominalPower, nominalWind;
-	public double maxVoltage,  maxWind;
-	public int offY,rayX, rayY, rayZ;
-	public int blockMalusSubCount;
-	public double blockMalus;
-	public String soundName;
-	public float nominalVolume;
+        voltageLevelColor = VoltageLevelColor.LowVoltage;
+    }
 
-	public FunctionTable PfW;
-	
-	public void draw(float alpha, boolean haloState) {
-		if(main != null) main.draw();
-		if(rot != null) rot.draw(alpha,1f,0f,0f);
-		if(halo != null) {
-			if(haloState){
-				UtilsClient.disableLight();
-				UtilsClient.enableBlend();
-				UtilsClient.disableCulling();
-				GL11.glColor3f(1.f, 0.f, 0.f);
-				halo.draw();
-				UtilsClient.enableCulling();
-				UtilsClient.disableBlend();
-				UtilsClient.enableLight();
-			}
-		}
-	}
+    public void setParent(net.minecraft.item.Item item, int damage) {
+        super.setParent(item, damage);
+        Data.addEnergy(newItemStack());
+    }
 
+    public void setGhostGroup(GhostGroup ghostGroup) {
+        blockMalusSubCount += ghostGroup.size();
+        this.ghostGroup = ghostGroup;
+    }
 
-	
-	@Override
-	public Direction getFrontFromPlace(Direction side,
-			EntityLivingBase entityLiving) {
-		return Direction.XN;
-	}
-	
-	
-	
-	@Override
-	public boolean handleRenderType(ItemStack item, ItemRenderType type) {
-		
-		return true;
-	}
-	
-	@Override
-	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item,
-			ItemRendererHelper helper) {
-		
-		return type != ItemRenderType.INVENTORY;
-	}
-	
-	@Override
-	public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
-		if (type == ItemRenderType.INVENTORY) {
-			super.renderItem(type, item, data);
-		} else {
-			objItemScale(obj);
-			Direction.ZN.glRotateXnRef();
-			draw(0f, false);
-		}
-	}
-	
-	@Override
-	public boolean use2DIcon() {
-		return true;
-	}
-	@Override
-	public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer,
-			List list, boolean par4) {
-		
-		super.addInformation(itemStack, entityPlayer, list, par4);
+    public void draw(float alpha, boolean haloState) {
+        if (main != null) main.draw();
+        if (rot != null) rot.draw(alpha, 1f, 0f, 0f);
+        if (halo != null) {
+            if (haloState) {
+                UtilsClient.disableLight();
+                UtilsClient.enableBlend();
+                UtilsClient.disableCulling();
+                GL11.glColor3f(1.f, 0.f, 0.f);
+                halo.draw();
+                UtilsClient.enableCulling();
+                UtilsClient.disableBlend();
+                UtilsClient.enableLight();
+            }
+        }
+    }
 
-		list.add(tr("Generates energy from wind."));
-		list.add(tr("Voltage: %1$V", Utils.plotValue(maxVoltage)));
-		list.add(tr("Power: %1$W", Utils.plotValue(nominalPower)));
-		list.add(tr("Wind area:"));
-		list.add("  " + tr("Front: %1$", rayX));
-		list.add("  " + tr("Up/Down: %1$", rayY));
-		list.add("  " + tr("Left/Right: %1$", rayZ));
-	}
+    @Override
+    public Direction getFrontFromPlace(Direction side,
+                                       EntityLivingBase entityLiving) {
+        return Direction.XN;
+    }
+
+    @Override
+    public boolean handleRenderType(ItemStack item, ItemRenderType type) {
+        return true;
+    }
+
+    @Override
+    public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item,
+                                         ItemRendererHelper helper) {
+        return type != ItemRenderType.INVENTORY;
+    }
+
+    @Override
+    public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
+        if (type == ItemRenderType.INVENTORY) {
+            super.renderItem(type, item, data);
+        } else {
+            objItemScale(obj);
+            Direction.ZN.glRotateXnRef();
+            draw(0f, false);
+        }
+    }
+
+    @Override
+    public boolean use2DIcon() {
+        return true;
+    }
+
+    @Override
+    public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer,
+                               List<String> list, boolean par4) {
+        super.addInformation(itemStack, entityPlayer, list, par4);
+
+        list.add(tr("Generates energy from wind."));
+        list.add(tr("Voltage: %1$V", Utils.plotValue(maxVoltage)));
+        list.add(tr("Power: %1$W", Utils.plotValue(nominalPower)));
+        list.add(tr("Wind area:"));
+        list.add("  " + tr("Front: %1$", rayX));
+        list.add("  " + tr("Up/Down: %1$", rayY));
+        list.add("  " + tr("Left/Right: %1$", rayZ));
+    }
 }
-
