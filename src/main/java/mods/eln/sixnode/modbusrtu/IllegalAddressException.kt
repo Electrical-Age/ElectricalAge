3043a793387d53e9b5b7cc06f27f8a3b2e76a633
cm0x4D <cm0x4d@codemonkey.ch>
2016-05-16 20:58:01 +0200

    Build-in minimal modbus implementation

---
+++ src/main/java/mods/eln/sixnode/modbusrtu/IllegalAddressException.kt
@@ -0,0 +1,4 @@
+package mods.eln.sixnode.modbusrtu
+
+class IllegalAddressException : Exception() {
+}
---
+++ src/main/java/mods/eln/sixnode/modbusrtu/ModbusTcpServer.kt
@@ -0,0 +1,180 @@
+package mods.eln.sixnode.modbusrtu
+
+import mods.eln.Eln
+import java.io.OutputStream
+import java.net.ServerSocket
+import java.net.Socket
+import java.nio.ByteBuffer
+import java.util.*
+
+class ModbusTcpServer(port: Int = 1502) {
+    companion object{
+        private val InputBufferSize = 256
+        private val OutputBufferSize = InputBufferSize
+    }
+
+    private val server = ServerSocket(port)
+    private val connections: MutableList<ConnectionHandler> = ArrayList()
+    private val slaves = TreeMap<Int, IModbusSlave>()
+
+    init {
+        if (Eln.modbusEnable) {
+            start()
+        }
+    }
+
+    private inner class ConnectionHandler(val socket: Socket) {
+        private val inputBuffer = ByteBuffer.allocate(InputBufferSize)
+
+        init {
+            start()
+        }
+
+        fun start() = Thread(Runnable {
+            while (!socket.isClosed) {
+                val size = socket.inputStream.read(inputBuffer.array(), inputBuffer.position(), inputBuffer.remaining())
+                if (size > 0) {
+                    inputBuffer.position(inputBuffer.position() + size).flip()
+                    handle(socket.outputStream)
+                    inputBuffer.flip()
+                } else {
+                    socket.close();
+                }
+            }
+        }).start()
+
+        private fun handle(output: OutputStream) {
+            while (inputBuffer.hasRemaining()) {
+                val transactionId = inputBuffer.short
+                if (inputBuffer.short == 0.toShort()) {
+                    val remaining = inputBuffer.short
+                    if (inputBuffer.remaining() >= remaining) {
+                        val slaveAddress = inputBuffer.get()
+                        val functionCode = inputBuffer.get()
+                        val response = ByteBuffer.allocate(OutputBufferSize)
+                                .putShort(transactionId).putShort(0).putShort(0).put(slaveAddress)
+                        val slave = slaves[slaveAddress.toInt()]
+                        if (slave != null) {
+                            synchronized(slave) {
+                                when (functionCode) {
+                                    0x01.toByte() -> readCoils(slave, response)
+                                    0x02.toByte() -> readDiscreteInputs(slave, response)
+                                    0x04.toByte() -> readInputRegisters(slave, response)
+                                    0x05.toByte() -> writeSingleCoil(slave, response)
+                                    0x06.toByte() -> writeSingleRegister(slave, response)
+                                    else -> {
+                                        response.put((0x80 + functionCode).toByte()).put(0x01.toByte())
+                                        inputBuffer.position(inputBuffer.position() + remaining - 2)
+                                    }
+                                }
+                            }
+                        } else {
+                            response.put((0x80 + functionCode).toByte()).put(0x0B.toByte())
+                            inputBuffer.position(inputBuffer.position() + remaining - 2)
+                        }
+                        response.putShort(4, (response.position() - 6).toShort())
+                        response.flip()
+                        output.write(response.array(), 0, response.remaining())
+                        output.flush()
+                    } else {
+                        return;
+                    }
+                } else {
+                    return;
+                }
+            }
+        }
+
+        private fun readCoils(slave: IModbusSlave, response: ByteBuffer) {
+            val address = inputBuffer.short
+            val quantity = inputBuffer.short
+
+            try {
+                // TODO: Support for multiple coils...
+                if (quantity == 1.toShort()) {
+                    val value = slave.getCoil(address.toInt());
+                    response.put(0x01.toByte()).put(1.toByte()).put((if (value) 1 else 0).toByte())
+                    return
+                }
+            } catch (e: IllegalAddressException) {}
+            response.put((0x81.toByte())).put(0x02.toByte())
+        }
+
+        private fun readDiscreteInputs(slave: IModbusSlave, response: ByteBuffer) {
+            val address = inputBuffer.short
+            val quantity = inputBuffer.short
+
+            try {
+                // TODO: Support for multiple inputs...
+                if (quantity == 1.toShort()) {
+                    val value = slave.getInput(address.toInt());
+                    response.put(0x02.toByte()).put(1.toByte()).put((if (value) 1 else 0).toByte())
+                    return
+                }
+            } catch (e: IllegalAddressException) {}
+            response.put((0x82.toByte())).put(0x02.toByte())
+        }
+
+        private fun readInputRegisters(slave: IModbusSlave, response: ByteBuffer) {
+            val address = inputBuffer.short
+            val quantity = inputBuffer.short
+
+            try {
+                // TODO: Support for multiple inputs registers...
+                if (quantity == 1.toShort()) {
+                    val value = slave.getInputRegister(address.toInt());
+                    response.put(0x04.toByte()).put(2.toByte()).putShort(value)
+                    return
+                }
+            } catch (e: IllegalAddressException) {}
+            response.put((0x84.toByte())).put(0x02.toByte())
+        }
+
+        private fun writeSingleCoil(slave: IModbusSlave, response: ByteBuffer) {
+            val address = inputBuffer.short
+            val value = inputBuffer.short
+
+            try {
+                slave.setCoil(address.toInt(), value == 0xFF00.toShort())
+                response.put(0x05.toByte()).putShort(address).putShort(value)
+                return
+            } catch (e: IllegalAddressException) {}
+            response.put((0x85.toByte())).put(0x02.toByte())
+        }
+
+        private fun writeSingleRegister(slave: IModbusSlave, response: ByteBuffer) {
+            val address = inputBuffer.short
+            val value = inputBuffer.short
+
+            try {
+                slave.setHoldingRegister(address.toInt(), value)
+                response.put(0x06.toByte()).putShort(address).putShort(value)
+                return
+            } catch (e: IllegalAddressException) {}
+            response.put((0x86.toByte())).put(0x02.toByte())
+        }
+
+        fun destroy() = socket.close()
+    }
+
+    fun start() = Thread(Runnable {
+        while (!server.isClosed) {
+            val socket = server.accept();
+            if (socket != null) {
+                connections.add(ConnectionHandler(socket))
+            }
+        }
+    }).start()
+
+    fun add(slave: IModbusSlave): Boolean {
+        slaves.put(slave.slaveId, slave)
+        return true
+    }
+
+    fun remove(slave: IModbusSlave) = slaves.remove(slave.slaveId)
+
+    fun destroy() {
+        server.close();
+        connections.forEach { it.destroy() }
+    }
+}
