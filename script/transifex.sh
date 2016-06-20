0cec06798d274f3af83d0698da6639b0f5ebcd34
Christopher Métrailler <christopher.metrailler@gmail.com>
2016-06-20 18:33:59 +0200

    Updated the Travis build for Transifex * Run in container-based infrastructure * Pip install in user home * Pip cache enabled

--- script/transifex.sh
+++ script/transifex.sh
@@ -5,18 +5,10 @@
 # Generate and push the source language file to Transifex.
 # This script is called after every successful build on Travis CI.
 # It relies on $TRANSIFEX_PASSWORD being set in .travis.yml
+# The Transifex client must be installed.
 
 if [ "$TRAVIS_REPO_SLUG" == "Electrical-Age/ElectricalAge" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "develop" ]
 then
-
-  echo "Installing the transifex client"
-  python --version
-  pip --version
-  
-  # Transifex-client version 0.12.1 is required
-  # See https://github.com/transifex/transifex-client/issues/113  
-  pip install transifex-client --upgrade
-
   echo "Generating the latest language source file from the develop branch"
   ./gradlew updateMasterLanguageFile
 
