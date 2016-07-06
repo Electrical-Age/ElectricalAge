308f4931472aecf5536de651aaefb62909504eb6
cm0x4D <cm0x4d@codemonkey.ch>
2016-07-06 18:46:49 +0200

    Fixes #456

--- src/main/java/mods/eln/misc/Version.java
+++ src/main/java/mods/eln/misc/Version.java
@@ -14,13 +14,13 @@ public final class Version {
 	public final static int MAJOR = 1;
 
 	/** Minor version code. */
-	public final static int MINOR = 10;
+	public final static int MINOR = 11;
 
 	/**
 	 * Unique version code. Must be a String for annotations. Used to check if a
 	 * new version if available. Each update must increment this number.
 	 */
-	public final static String REVISION = "51";
+	public final static String REVISION = "52";
 
 	public final static String getVersionName() {
 		return String.format("BETA-%d.%d r%s", MAJOR, MINOR, REVISION);
