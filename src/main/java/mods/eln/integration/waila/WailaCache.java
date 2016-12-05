bf5907364655f1388e2969858f19de092bf93988
cm0x4D <cm0x4d@codemonkey.ch>
2016-12-05 23:53:10 +0100

    Using cache correctly for ghost and sixnodes
    @bloxgate Any objections to adapt transparent nodes too?

--- src/main/java/mods/eln/integration/waila/WailaCache.java
+++ src/main/java/mods/eln/integration/waila/WailaCache.java
@@ -3,6 +3,8 @@ package mods.eln.integration.waila;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import mods.eln.Eln;
 import mods.eln.misc.Coordonate;
 import mods.eln.packets.GhostNodeWailaRequestPacket;
@@ -30,25 +32,37 @@ public class WailaCache {
         );
 
     public static LoadingCache<SixNodeCoordonate, Map<String, String>> sixNodes = CacheBuilder.newBuilder()
-        .maximumSize(200)
-        .expireAfterWrite(5, TimeUnit.SECONDS)
+        .maximumSize(20)
+        .refreshAfterWrite(2, TimeUnit.SECONDS)
         .build(
             new CacheLoader<SixNodeCoordonate, Map<String, String>>() {
                 public Map<String, String> load(SixNodeCoordonate key) throws Exception {
                     Eln.elnNetwork.sendToServer(new SixNodeWailaRequestPacket(key.getCoord(), key.getSide()));
                     return null;
                 }
+
+                @Override
+                public ListenableFuture<Map<String, String>> reload(SixNodeCoordonate key, Map<String, String> oldValue) throws Exception {
+                    load(key);
+                    return Futures.immediateFuture(oldValue);
+                }
             }
         );
 
     public static LoadingCache<Coordonate, GhostNodeWailaData> ghostNodes = CacheBuilder.newBuilder()
-        .maximumSize(200)
-        .expireAfterWrite(1, TimeUnit.MINUTES)
+        .maximumSize(20)
+        .refreshAfterWrite(10, TimeUnit.SECONDS)
         .build(
             new CacheLoader<Coordonate, GhostNodeWailaData>() {
                 public GhostNodeWailaData load(Coordonate key) throws Exception {
                     Eln.elnNetwork.sendToServer(new GhostNodeWailaRequestPacket(key));
                     return null;
+                }
+
+                @Override
+                public ListenableFuture<GhostNodeWailaData> reload(Coordonate key, GhostNodeWailaData oldValue) throws Exception {
+                    load(key);
+                    return Futures.immediateFuture(oldValue);
                 }
             }
         );
