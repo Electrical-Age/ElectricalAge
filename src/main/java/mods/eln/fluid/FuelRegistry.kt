ab97398b6439d196c79f289c639e0a9f474a2297
cm0x4D <cm0x4d@codemonkey.ch>
2016-12-28 23:02:33 +0100

    Added heat value to fuel registry and global factor
    Global factor is still a bogus value. This needs some adjustment.

--- src/main/java/mods/eln/fluid/FuelRegistry.kt
+++ src/main/java/mods/eln/fluid/FuelRegistry.kt
@@ -1,43 +1,58 @@
 package mods.eln.fluid
 
+import mods.eln.Eln
+import net.minecraftforge.fluids.Fluid
 import net.minecraftforge.fluids.FluidRegistry
 
 class FuelRegistry {
     companion object {
+        /**
+         * Diesel is a refined, heavy fuel, can only be used by the fuel heat furnace for the moment.
+         *
+         * The values represent the heating value (energy) for 1l of the fuel IRL.
+         */
+        private val dieselFuels = mapOf(
+                "biodiesel" to 32560000.0,  // Immersive Engineering, density = 0.88 kg/l, heating value = 37 MJ/kg
+                "heavyoil" to 39100000.0    // Magneticraft, heating value = 39.1 MJ/l
+        )
+        val dieselList = dieselFuels.keys.toTypedArray()
 
-    /**
-     * Diesel is a refined, heavy fuel, and not usable anywhere yet.
-     */
-    val dieselList = arrayOf(
-            "biodiesel", // Immersive Engineering
-            "heavyoil"  // Magneticraft
-    )
+        /**
+         * Gasoline-equivalents: Light oils, the type which can reasonably be burned by internal combustion engines or gas turbines.
+         * The ones on this list are all pretty close to each other in energy content.
+         *
+         * The values represent the heating value (energy) for 1l of the fuel IRL.
+         */
+        private val gasolineFuels = mapOf(
+                "fuel" to 31570000.0,       // Buildcraft, density = 0.77 kg/l, heating value = 41 MJ/kg
+                "rc ethanol" to 21172000.0, // RotaryCraft, density = 0,79 kg/l, heating value = 26.8 MJ/kg
+                "biofuel" to 17826480.0,    // Minefactory Reloaded, (Bioethanol) density = 0.786 kg/l, heating value = 22,68 MJ/l
+                "bioethanol" to 17826480.0, // Forestry
+                "kerosene" to 34800000.0,   // PneumaticCraft, heat value = 34,8 MJ/l
+                "lpg" to 24840000.0,        // PneumaticCraft, density = 0.54 kg/l, heat value = 46 MJ/kg
+                "fuelgc" to 31570000.0,     // GalactiCraft, see "fuel"
+                "lightoil" to 35358000.0    // Magneticraft, density = 0.83 kg/l, heating value = 42.6 MJ/kg
+        )
+        val gasolineList = gasolineFuels.keys.toTypedArray()
 
-    /**
-     * Gasoline-equivalents: Light oils, the type which can reasonably be burned by internal combustion engines or gas turbines.
-     * The ones on this list are all pretty close to each other in energy content.
-     */
-    val gasolineList = arrayOf(
-            // Various gasoline equivalents, and light oils a small turbine can reasonably burn.
-            "syngas", // Advanced Generators
-            "fuel", // Buildcraft
-            "rc ethanol", // RotaryCraft
-            "biofuel", // Minefactory Reloaded
-            "bioethanol", // Forestry
-            "kerosene", // PneumaticCraft
-            "lpg", // PneumaticCraft
-            "fuelgc", // GalactiCraft
-            "lightoil"  // Magneticraft
-    )
+        /**
+         * Burnable gases. Gas turbine is still happy, fuel generator is not.
+         *
+         * The values represent the heating value (energy) for 1l of the fuel IRL.
+         */
+        private val gasFuels = mapOf(
+                "naturalgas" to 36000.0,    // Magneticraft, heating value = 36 MJ/m3
+                "syngas" to 20000.0         // Advanced Generators, heating value = 20 MJ/m3
+        )
+        val gasList = gasFuels.keys.toTypedArray()
 
-    /**
-     * Burnable gases. Gas turbine is still happy, fuel generator is not.
-     */
-    val gasList = arrayOf(
-            "naturalgas"  // Magneticraft
-    )
+        // All fuels together.
+        private val allFuels = dieselFuels + gasolineFuels + gasFuels
 
-    fun fluidListToFluids(fluidNames: Array<String>) =
-            fluidNames.map { FluidRegistry.getFluid(it) }.filterNotNull().toTypedArray()
+        fun fluidListToFluids(fluidNames: Array<String>) =
+                fluidNames.map { FluidRegistry.getFluid(it) }.filterNotNull().toTypedArray()
+
+        fun heatEnergyPerMilliBucket(fuelName: String) : Double = Eln.fuelHeatValueFactor * (allFuels[fuelName] ?: 0.0)
+        fun heatEnergyPerMilliBucket(fluid: Fluid?) : Double = heatEnergyPerMilliBucket(fluid?.name ?: "")
     }
 }
