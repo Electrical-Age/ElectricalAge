8b39defc2a47903219940c5ef32032e078b201b7
cm0x4D <cm0x4d@codemonkey.ch>
2016-12-29 23:37:33 +0100

    Fixes #574

---
+++ src/main/java/mods/eln/integration/waila/SixNodeWailaData.kt
@@ -0,0 +1,5 @@
+package mods.eln.integration.waila
+
+import net.minecraft.item.ItemStack
+
+data class SixNodeWailaData(val itemStack: ItemStack?, val data: Map<String, String>)
--- src/main/java/mods/eln/packets/SixNodeWailaResponsePacket.kt
+++ src/main/java/mods/eln/packets/SixNodeWailaResponsePacket.kt
@@ -1,25 +1,31 @@
 package mods.eln.packets
 
+import cpw.mods.fml.common.network.ByteBufUtils
 import io.netty.buffer.ByteBuf
 import mods.eln.misc.Coordonate
 import mods.eln.misc.Direction
+import net.minecraft.item.ItemStack
 
 class SixNodeWailaResponsePacket: TransparentNodeResponsePacket {
     lateinit var side: Direction
+    var itemStack: ItemStack? = null
 
     constructor() {}
 
-    constructor(coord: Coordonate, side: Direction, data: Map<String,String>): super(data, coord) {
+    constructor(coord: Coordonate, side: Direction, itemStack: ItemStack?, data: Map<String,String>): super(data, coord) {
         this.side = side
+        this.itemStack = itemStack
     }
 
     override fun fromBytes(buf: ByteBuf?) {
         super.fromBytes(buf)
         side = Direction.fromInt(buf?.readInt() ?: 0)
+        itemStack = ByteBufUtils.readItemStack(buf)
     }
 
     override fun toBytes(buf: ByteBuf?) {
         super.toBytes(buf)
         buf?.writeInt(side.int)
+        ByteBufUtils.writeItemStack(buf, itemStack)
     }
 }
