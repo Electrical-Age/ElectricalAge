bb4e753a02155a6443aafebf4efda5472175f39c
cm0x4D <cm0x4d@codemonkey.ch>
2017-01-04 20:46:03 +0100

    Using "Plan B" without inventory on client side

--- src/main/java/mods/eln/item/FuelBurnerDescriptor.kt
+++ src/main/java/mods/eln/item/FuelBurnerDescriptor.kt
@@ -10,12 +10,19 @@ import net.minecraft.item.ItemStack
 class FuelBurnerDescriptor(name: String, val  producedHeatPower: Double, val type: Int, val soundPitch: Float) :
         GenericItemUsingDamageDescriptorUpgrade(name) {
     companion object {
+        private val descriptors: MutableMap<Int, FuelBurnerDescriptor> = mutableMapOf()
+
+        fun powerForType(type: Int?) = descriptors.get(type ?: -1)?.producedHeatPower ?: 0.0
+
+        fun pitchForType(type: Int?) = descriptors.get(type ?: -1)?.soundPitch ?: 1f
+
         fun getDescriptor(itemStack: ItemStack?) =
                 (itemStack?.item as? GenericItemUsingDamage<*>)?.getDescriptor(itemStack) as? FuelBurnerDescriptor
     }
 
     init {
         voltageLevelColor = VoltageLevelColor.Neutral
+        FuelBurnerDescriptor.descriptors[type] = this
     }
 
     override fun addInformation(itemStack: ItemStack?, entityPlayer: EntityPlayer?, list: MutableList<Any?>, par4: Boolean) {
