8b04f890ff0cb64ee47ebfd2c660aee2a542ae60
cm0x4D <cm0x4d@codemonkey.ch>
2017-01-14 22:40:51 +0100

    New sample looping for dc/dc
    + code cleanup

--- src/main/java/mods/eln/transparentnode/transformer/TransformerGuiDraw.java
+++ src/main/java/mods/eln/transparentnode/transformer/TransformerGuiDraw.java
@@ -5,78 +5,55 @@ import mods.eln.gui.GuiContainerEln;
 import mods.eln.gui.GuiHelperContainer;
 import mods.eln.gui.IGuiObject;
 import mods.eln.node.transparent.TransparentNodeElementInventory;
-import net.minecraft.client.gui.GuiButton;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 
-
 public class TransformerGuiDraw extends GuiContainerEln {
+    private final TransparentNodeElementInventory inventory;
+    private final TransformerRender render;
+    private GuiButtonEln isIsolator;
 
-	
-    private TransparentNodeElementInventory inventory;
-    TransformerRender render;
-    GuiButtonEln isIsolator;
-
-    
-    
-    public TransformerGuiDraw(EntityPlayer player, IInventory inventory,TransformerRender render)
-    {
+    public TransformerGuiDraw(EntityPlayer player, IInventory inventory, TransformerRender render) {
         super(new TransformerContainer(player, inventory));
         this.inventory = (TransparentNodeElementInventory) inventory;
         this.render = render;
-        
-      
     }
-    
-    public void initGui()
-    {
-    	super.initGui();
-    	
-    
-    	isIsolator = newGuiButton(176/2-36,8+3+60,72 , "");
-        isIsolator.setComment(0,"Could be used to improve");
-        isIsolator.setComment(1,"Electrical Age simulation time");
-        isIsolator.setComment(2,"When isolated is selected");
-        isIsolator.setComment(3,"it split the network at this place");
-        isIsolator.setComment(4,"Usefull to isolate stuff that");
-        isIsolator.setComment(5,"switch very very often like relay");
-        isIsolator.setComment(5,"in a DC/DC circuit");
-        isIsolator.setComment(6,"The downside of this mode is that");
-        isIsolator.setComment(7,"will react like a little capacitor");
 
+    public void initGui() {
+        super.initGui();
+
+        isIsolator = newGuiButton(176 / 2 - 36, 8 + 3 + 60, 72, "");
+        isIsolator.setComment(0, "Can be used to improve");
+        isIsolator.setComment(1, "simulation performance.");
+        isIsolator.setComment(2, "When isolated is selected");
+        isIsolator.setComment(3, "the network will be split.");
+        isIsolator.setComment(4, "Useful to isolate circuits that");
+        isIsolator.setComment(5, "switch very very often like");
+        isIsolator.setComment(5, "relays in a DC/DC circuit.");
+        isIsolator.setComment(6, "The downside of this mode is that");
+        isIsolator.setComment(7, "it adds a little capacitance.");
     }
-    
 
     @Override
     protected void preDraw(float f, int x, int y) {
-    	
-    	super.preDraw(f, x, y);
-    	if(render.isIsolator)
-    		isIsolator.displayString = "Isolated";
-    	else
-    		isIsolator.displayString = "Not isolated";
-
+        super.preDraw(f, x, y);
+        if (render.isIsolator)
+            isIsolator.displayString = "Isolated";
+        else
+            isIsolator.displayString = "Not isolated";
     }
 
     @Override
     public void guiObjectEvent(IGuiObject object) {
-    	
-    	super.guiObjectEvent(object);
-    	if(object == isIsolator)
-    	{
-    		render.clientSendId(TransformerElement.toogleIsIsolator);
-    	}
+        super.guiObjectEvent(object);
+
+        if (object == isIsolator) {
+            render.clientSendId(TransformerElement.toogleIsIsolator);
+        }
     }
 
-	@Override
-	protected GuiHelperContainer newHelper() {
-		
-			return new GuiHelperContainer(this, 176, 194-33+20,8,84 + 194 - 166-33+20, "transformer.png");
-		//return new HelperStdContainer(this);
-	}
-    
-
-    
-    
-
+    @Override
+    protected GuiHelperContainer newHelper() {
+        return new GuiHelperContainer(this, 176, 194 - 33 + 20, 8, 84 + 194 - 166 - 33 + 20, "transformer.png");
+    }
 }
