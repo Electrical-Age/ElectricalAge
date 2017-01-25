b3e0a669560b3723c820674e0a791c9062a91239
cm0x4D <cm0x4d@codemonkey.ch>
2017-01-26 00:08:04 +0100

    New sample looping for turbines
    (and some code cleanup)

--- src/main/java/mods/eln/transparentnode/turbine/TurbineThermalProcess.java
+++ src/main/java/mods/eln/transparentnode/turbine/TurbineThermalProcess.java
@@ -3,38 +3,30 @@ package mods.eln.transparentnode.turbine;
 import mods.eln.Eln;
 import mods.eln.sim.IProcess;
 import mods.eln.sim.PhysicalConstant;
-import mods.eln.sim.mna.component.PowerSource;
 import mods.eln.sim.mna.component.VoltageSource;
-import mods.eln.sound.SoundCommand;
-import mods.eln.sound.SoundLooper;
 
 
-public class TurbineThermalProcess implements IProcess{
-	TurbineElement turbine;
+public class TurbineThermalProcess implements IProcess {
+    private final TurbineElement turbine;
 
-	public TurbineThermalProcess(TurbineElement t) {
-		this.turbine = t;
-	}
-	
+    public TurbineThermalProcess(TurbineElement t) {
+        this.turbine = t;
+    }
 
-	
-	
-	@Override
-	public void process(double time) {
-		TurbineDescriptor descriptor = turbine.descriptor;
+    @Override
+    public void process(double time) {
+        TurbineDescriptor descriptor = turbine.descriptor;
 
-		//PowerSource eps = turbine.electricalPowerSourceProcess;
-		VoltageSource src = turbine.electricalPowerSourceProcess;
+        VoltageSource src = turbine.electricalPowerSourceProcess;
 
-		double eff  = Math.abs(1 - (turbine.coolLoad.Tc + PhysicalConstant.Tref)/(turbine.warmLoad.Tc + PhysicalConstant.Tref));
-		if(eff < 0.05) eff = 0.05;
+        double eff = Math.abs(1 - (turbine.coolLoad.Tc + PhysicalConstant.Tref) / (turbine.warmLoad.Tc + PhysicalConstant.Tref));
+        if (eff < 0.05) eff = 0.05;
 
-		double E = src.getP()*time / Eln.instance.heatTurbinePowerFactor;
+        double E = src.getP() * time / Eln.instance.heatTurbinePowerFactor;
 
-		double Pout = E/time;
-		double Pin = descriptor.PoutToPin.getValue(Pout) / eff;
-		turbine.warmLoad.movePowerTo(-Pin);
-		turbine.coolLoad.movePowerTo(Pin * (1 - eff));	
-	}
-
+        double Pout = E / time;
+        double Pin = descriptor.PoutToPin.getValue(Pout) / eff;
+        turbine.warmLoad.movePowerTo(-Pin);
+        turbine.coolLoad.movePowerTo(Pin * (1 - eff));
+    }
 }
