a04c3b8ea334d1c61524e313875ee604b2b0a309
cm0x4D <cm0x4d@codemonkey.ch>
2017-01-29 22:45:42 +0100

    Added animation and (incomplete) GUI to fridge

--- src/main/java/mods/eln/transparentnode/Fridge.kt
+++ src/main/java/mods/eln/transparentnode/Fridge.kt
@@ -1,12 +1,19 @@
 package mods.eln.transparentnode
 
-import mods.eln.misc.Direction
-import mods.eln.misc.LRDU
-import mods.eln.misc.Obj3D
+import mods.eln.gui.GuiContainerEln
+import mods.eln.gui.GuiHelperContainer
+import mods.eln.gui.ISlotSkin.SlotSkin
+import mods.eln.gui.SlotWithSkin
+import mods.eln.misc.*
+import mods.eln.node.NodeBase
 import mods.eln.node.transparent.*
 import mods.eln.sim.ElectricalLoad
 import mods.eln.sim.ThermalLoad
+import mods.eln.sim.nbt.NbtElectricalLoad
+import mods.eln.sim.nbt.NbtResistor
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.inventory.IInventory
+import net.minecraft.inventory.Slot
 
 class FridgeDescriptor(name: String, obj: Obj3D) :
         TransparentNodeDescriptor(name, FridgeElement::class.java, FridgeRender::class.java) {
@@ -23,28 +30,71 @@ class FridgeDescriptor(name: String, obj: Obj3D) :
 
 class FridgeElement(node: TransparentNode, descriptor: TransparentNodeDescriptor) :
         TransparentNodeElement(node, descriptor) {
-    override fun initialize() {}
-    override fun getConnectionMask(side: Direction?, lrdu: LRDU?) = 0
-    override fun getElectricalLoad(side: Direction?, lrdu: LRDU?): ElectricalLoad? = null
-    override fun getThermalLoad(side: Direction?, lrdu: LRDU?): ThermalLoad? = null
-    override fun multiMeterString(side: Direction?) = ""
-    override fun thermoMeterString(side: Direction?) = ""
-    override fun onBlockActivated(entityPlayer: EntityPlayer?, side: Direction?, vx: Float, vy: Float, vz: Float) = false
+    private val load = NbtElectricalLoad("load")
+    private val resistor = NbtResistor("resistor", load, null)
+
+    private val inventory = TransparentNodeElementInventory(36, 1, this)
+
+    init {
+        electricalLoadList.add(load)
+        electricalComponentList.add(resistor)
+    }
+
+    override fun initialize() {
+        connect()
+    }
+
+    override fun getConnectionMask(side: Direction, lrdu: LRDU) = when (side) {
+        front.inverse, front.left(), front.right() -> NodeBase.maskElectricalPower
+        else -> 0
+    }
+
+    override fun getElectricalLoad(side: Direction, lrdu: LRDU): ElectricalLoad? = load
+
+    override fun getThermalLoad(side: Direction, lrdu: LRDU): ThermalLoad? = null
+
+    override fun multiMeterString(side: Direction) = ""
+
+    override fun thermoMeterString(side: Direction) = ""
+
+    override fun onBlockActivated(entityPlayer: EntityPlayer?, side: Direction, vx: Float, vy: Float, vz: Float) = false
+
+    override fun hasGui() = true
+
+    override fun getInventory() = inventory
+
+    override fun newContainer(side: Direction, player: EntityPlayer) = FridgeContainer(player, inventory)
 }
 
 class FridgeRender(entity: TransparentNodeEntity, descriptor: TransparentNodeDescriptor) :
         TransparentNodeElementRender(entity, descriptor) {
     private val descriptor = descriptor as FridgeDescriptor
-    var open = 0f
+
+    private val coord = Coordonate(entity)
+
+    private val inventory = TransparentNodeElementInventory(36, 1, this)
+
+    private var open = PhysicalInterpolator(0.4f, 8f, 0.4f, 0f)
 
     override fun draw() {
         front.left().glRotateZnRef()
-        descriptor.draw(open)
+        descriptor.draw(open.get())
     }
 
     override fun refresh(deltaT: Float) {
         super.refresh(deltaT)
-        open += deltaT / 5
-        if (open > 1) open = 0f
+        open.target = if (Utils.isPlayerAround(tileEntity.worldObj, coord.moved(front).getAxisAlignedBB(0))) 1f else 0f
+        open.step(deltaT)
     }
+
+    override fun newGuiDraw(side: Direction, player: EntityPlayer) = FridgeGui(player, inventory, this)
 }
+
+class FridgeGui(player: EntityPlayer, inventory: IInventory, render: FridgeRender) : GuiContainerEln(FridgeContainer(player, inventory)) {
+    override fun newHelper(): GuiHelperContainer = GuiHelperContainer(this, 176, 186, 8, 104)
+}
+
+class FridgeContainer(player: EntityPlayer, inventory: IInventory)
+    : BasicContainer(player, inventory, Array<Slot>(32, {
+    SlotWithSkin(inventory, it, 26 + it.mod(8) * 18, 8 + (if (it >= 8) 10 else 0) + (it / 8) * 18, SlotSkin.medium)
+})) {}
