f8feb34c791e0a4aac984725f6cec9877b3b0867
Svein Ove Aas <sveina@gmail.com>
2017-02-01 21:28:50 +0000

    Swap UuidManager list for hashmap, with plenty of fatals

--- src/main/java/mods/eln/client/UuidManager.kt
+++ src/main/java/mods/eln/client/UuidManager.kt
@@ -4,49 +4,43 @@ import cpw.mods.fml.common.FMLCommonHandler
 import cpw.mods.fml.common.eventhandler.SubscribeEvent
 import cpw.mods.fml.common.gameevent.TickEvent
 import cpw.mods.fml.common.gameevent.TickEvent.Phase
-
-import java.util.ArrayList
-import java.util.LinkedList
+import mods.eln.misc.Utils
+import net.minecraft.launchwrapper.LogWrapper.log
+import java.util.*
 
 class UuidManager {
-    internal var eList = LinkedList<Pair>()
-
-    class Pair internal constructor(internal var uuid: ArrayList<Int>?, internal var e: IUuidEntity)
+    internal val entities = HashMap <Int, IUuidEntity>()
 
     init {
         FMLCommonHandler.instance().bus().register(this)
     }
 
     fun add(uuid: ArrayList<Int>, e: IUuidEntity) {
-        eList.add(Pair(uuid, e))
+        if (uuid.size != 1) {
+            Utils.fatal()
+        }
+        val uuid = uuid.single()
+        if (entities.containsKey(uuid)) {
+            Utils.fatal()
+        }
+        entities.put(uuid, e)
     }
 
     @SubscribeEvent
     fun tick(event: TickEvent.ClientTickEvent) {
         if (event.phase == Phase.END) return
 
-        val i = eList.iterator()
+        val i = entities.iterator()
 
         while (i.hasNext()) {
             val p = i.next()
-            if (!p.e.isAlive) {
+            if (!p.value.isAlive) {
                 i.remove()
             }
         }
-        //Utils.println(eList.size());
     }
 
     fun kill(uuid: Int) {
-        val i = eList.iterator()
-        while (i.hasNext()) {
-            val p = i.next()
-            if (p.uuid == null) continue
-            for (pUuid in p.uuid!!) {
-                if (pUuid === uuid) {
-                    p.e.kill()
-                    i.remove()
-                }
-            }
-        }
+        entities.remove(uuid)?.kill()
     }
 }
