143b785bb7b8ff9da04b94a3bdb1df1ecffb437e
Svein Ove Aas <sveina@gmail.com>
2017-02-22 21:45:22 +0000

    Switch all file-reading to the NIO API

--- src/main/java/mods/eln/server/ServerEventListener.java
+++ src/main/java/mods/eln/server/ServerEventListener.java
@@ -19,9 +19,8 @@ import net.minecraftforge.event.world.WorldEvent.Load;
 import net.minecraftforge.event.world.WorldEvent.Save;
 import net.minecraftforge.event.world.WorldEvent.Unload;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.nio.file.*;
 import java.util.HashSet;
 import java.util.LinkedList;
 
@@ -78,24 +77,24 @@ public class ServerEventListener {
         FileNames fileNames = new FileNames(e);
 
         try {
-            FileInputStream fileStream = new FileInputStream(fileNames.worldSaveName);
-            NBTTagCompound nbt = CompressedStreamTools.readCompressed(fileStream);
-            readFromEaWorldNBT(nbt);
-            fileStream.close();
+            readSave(fileNames.worldSave);
         } catch (Exception ex) {
             try {
                 ex.printStackTrace();
-                FileInputStream fileStream = new FileInputStream(fileNames.backupSaveName);
-                System.out.println("Using BACKUP Electrical Age save: " + fileNames.backupSaveName);
-                NBTTagCompound nbt = CompressedStreamTools.readCompressed(fileStream);
-                readFromEaWorldNBT(nbt);
-                fileStream.close();
+                System.out.println("Using BACKUP Electrical Age save: " + fileNames.backupSave);
+                readSave(fileNames.backupSave);
             } catch (Exception ex2) {
                 ex2.printStackTrace();
                 System.out.println("Failed to read backup save!");
                 ElnWorldStorage storage = ElnWorldStorage.forWorld(e.world);
             }
         }
+    }
+
+    private void readSave(Path worldSave) throws IOException {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(Files.readAllBytes(worldSave));
+        NBTTagCompound nbt = CompressedStreamTools.readCompressed(inputStream);
+        readFromEaWorldNBT(nbt);
     }
 
     @SubscribeEvent
@@ -123,24 +122,25 @@ public class ServerEventListener {
             writeToEaWorldNBT(nbt, e.world.provider.dimensionId);
 
             FileNames fileNames = new FileNames(e);
-            String tempSaveName = fileNames.tempSaveName;
-            String worldSaveName = fileNames.worldSaveName;
-            String backupSaveName = fileNames.backupSaveName;
 
-            File failedSave = new File(tempSaveName);
-            if (failedSave.exists()) {
-                failedSave.delete();
-            }
+            // Write a new save to a temporary file.
+            final ByteArrayOutputStream bytes = new ByteArrayOutputStream(512 * 1024);
+            CompressedStreamTools.writeCompressed(nbt, bytes);
+            Files.write(fileNames.tempSave, bytes.toByteArray());
 
-            FileOutputStream fileStream = new FileOutputStream(tempSaveName);
-            CompressedStreamTools.writeCompressed(nbt, fileStream);
-            fileStream.flush();
-            fileStream.close();
-
-            new File(worldSaveName).renameTo(new File(backupSaveName));
-            new File(tempSaveName).renameTo(new File(worldSaveName));
+            // Replace backup save with old save, and old save with new one.
+            replaceFile(fileNames.worldSave, fileNames.backupSave);
+            replaceFile(fileNames.tempSave, fileNames.worldSave);
         } catch (Exception ex) {
             ex.printStackTrace();
+        }
+    }
+
+    private void replaceFile(Path from, Path to) throws IOException {
+        try {
+            Files.move(from, to, StandardCopyOption.ATOMIC_MOVE);
+        } catch (AtomicMoveNotSupportedException e) {
+            Files.move(from, to, StandardCopyOption.REPLACE_EXISTING);
         }
     }
 
@@ -173,14 +173,15 @@ public class ServerEventListener {
     }
 
     private class FileNames {
-        final String worldSaveName;
-        final String tempSaveName;
-        final String backupSaveName;
+        final Path worldSave;
+        final Path tempSave;
+        final Path backupSave;
 
         FileNames(WorldEvent e) {
-            worldSaveName = getEaWorldSaveName(e.world);
-            tempSaveName = worldSaveName + ".tmp";
-            backupSaveName = worldSaveName + ".bak";
+            String saveName = getEaWorldSaveName(e.world);
+            worldSave = FileSystems.getDefault().getPath(saveName);
+            tempSave = FileSystems.getDefault().getPath(saveName + ".tmp");
+            backupSave = FileSystems.getDefault().getPath(saveName + ".bak");
         }
     }
 }
