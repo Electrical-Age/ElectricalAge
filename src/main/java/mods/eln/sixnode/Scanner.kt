b1747c2f5dab394e524714fff1c850f83a3ca419
Svein Ove Aas <sveina@gmail.com>
2017-03-03 02:46:00 +0000

    Scanner: Initial implementation

--- src/main/java/mods/eln/sixnode/Scanner.kt
+++ src/main/java/mods/eln/sixnode/Scanner.kt
@@ -2,12 +2,18 @@ package mods.eln.sixnode
 
 import mods.eln.Eln
 import mods.eln.cable.CableRenderDescriptor
+import mods.eln.misc.Coordonate
 import mods.eln.misc.Direction
 import mods.eln.misc.LRDU
 import mods.eln.misc.Obj3D
 import mods.eln.node.NodeBase
 import mods.eln.node.six.*
+import mods.eln.sim.IProcess
+import mods.eln.sim.nbt.NbtElectricalGateOutput
+import mods.eln.sim.nbt.NbtElectricalGateOutputProcess
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.inventory.IInventory
+import net.minecraftforge.fluids.IFluidHandler
 
 /**
  * A comparator-alike. It doesn't "compare" anything, though.
@@ -24,7 +30,39 @@ class ScannerDescriptor(name: String, obj: Obj3D) : SixNodeDescriptor(name, Scan
 
 
 class ScannerElement(sixNode: SixNode, side: Direction, descriptor: SixNodeDescriptor) : SixNodeElement(sixNode, side, descriptor) {
-    override fun getElectricalLoad(lrdu: LRDU?) = null
+
+    val output = NbtElectricalGateOutput("signal")
+    val outputProcess = NbtElectricalGateOutputProcess("signalP", output)
+
+    val scannedCoord = Coordonate(1, 0, 0, 0).apply {
+        applyTransformation(side.down(), coordonate)
+    }
+
+    val updater = IProcess {
+        val te = scannedCoord.tileEntity
+        // TODO: Throttling.
+        val out: Double
+        if (te is IFluidHandler) {
+            val info = te.getTankInfo(side.inverse.toForge())
+            out = info.sumByDouble {
+                (it.fluid?.amount ?: 0).toDouble() / it.capacity
+            } / info.size
+        } else if (te is IInventory) {
+            val sum = (0..te.sizeInventory - 1).sumBy { te.getStackInSlot(it)?.stackSize ?: 0 }
+            out = sum.toDouble() / te.inventoryStackLimit / te.sizeInventory
+        } else {
+            out = scannedCoord.block.lightValue / 15.0
+        }
+        outputProcess.outputNormalized = out
+    }
+
+    init {
+        electricalLoadList.add(output)
+        electricalComponentList.add(outputProcess)
+        slowProcessList.add(updater)
+    }
+
+    override fun getElectricalLoad(lrdu: LRDU?) = output
     override fun getThermalLoad(lrdu: LRDU?) = null
 
     override fun getConnectionMask(lrdu: LRDU) = when (lrdu) {
@@ -41,6 +79,7 @@ class ScannerElement(sixNode: SixNode, side: Direction, descriptor: SixNodeDescr
 
     override fun onBlockActivated(entityPlayer: EntityPlayer?, side: Direction?, vx: Float, vy: Float, vz: Float) = false
 }
+
 
 
 class ScannerRender(entity: SixNodeEntity, side: Direction, descriptor: SixNodeDescriptor) : SixNodeElementRender(entity, side, descriptor) {
