2e938020444fe7f658572f9adb75eb042bd2764a
Svein Ove Aas <sveina@gmail.com>
2017-03-03 16:48:00 +0000

    Add slots mode, fix directions *again*, add tooltip

--- src/main/java/mods/eln/sixnode/Scanner.kt
+++ src/main/java/mods/eln/sixnode/Scanner.kt
@@ -2,14 +2,17 @@ package mods.eln.sixnode
 
 import mods.eln.Eln
 import mods.eln.cable.CableRenderDescriptor
+import mods.eln.i18n.I18N.tr
 import mods.eln.misc.*
 import mods.eln.node.NodeBase
 import mods.eln.node.six.*
 import mods.eln.sim.IProcess
 import mods.eln.sim.nbt.NbtElectricalGateOutput
 import mods.eln.sim.nbt.NbtElectricalGateOutputProcess
+import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.inventory.IInventory
 import net.minecraft.inventory.ISidedInventory
+import net.minecraft.item.ItemStack
 import net.minecraft.tileentity.TileEntity
 import net.minecraftforge.common.util.ForgeDirection
 import net.minecraftforge.fluids.IFluidHandler
@@ -25,73 +28,109 @@ class ScannerDescriptor(name: String, obj: Obj3D) : SixNodeDescriptor(name, Scan
         main.draw()
     }
 
+    override fun addInformation(itemStack: ItemStack?, entityPlayer: EntityPlayer?, list: MutableList<String>, par4: Boolean) {
+        super.addInformation(itemStack, entityPlayer, list, par4)
+        list.add(tr("Scans blocks to produce signals."))
+        list.add(tr("- For tanks, outputs fill percentage."))
+        list.add(tr("- For inventories, outputs either total fill or fraction of slots with any items."))
+        list.add(tr("Right-click to change mode."))
+        list.add(tr("Otherwise behaves as a vanilla comparator."))
+    }
 }
 
+enum class ScanMode {
+    SIMPLE, SLOTS
+}
 
 class ScannerElement(sixNode: SixNode, side: Direction, descriptor: SixNodeDescriptor) : SixNodeElement(sixNode, side, descriptor) {
-
     val output = NbtElectricalGateOutput("signal")
     val outputProcess = NbtElectricalGateOutputProcess("signalP", output)
 
-    val forgeSide: ForgeDirection = side.inverse.toForge()
+    var mode = ScanMode.SIMPLE
 
     val updater = IProcess {
+        val appliedLRDU = side.applyLRDU(front)
         val scannedCoord = Coordonate(coordonate).apply {
-            move(side.applyLRDU(front))
+            move(appliedLRDU)
         }
+        val targetSide: ForgeDirection = appliedLRDU.inverse.toForge()
         val te = scannedCoord.tileEntity
         // TODO: Throttling.
-        var out = 0.0
+        var out = -1.0
         if (te != null) {
-            out = scanTileEntity(te)
+            out = scanTileEntity(te, targetSide)
         }
-        if (out == 0.0) {
-            out = scanBlock(scannedCoord)
+        if (out == -1.0) {
+            out = scanBlock(scannedCoord, targetSide)
         }
         outputProcess.outputNormalized = out
-    }
-
-    private fun scanBlock(scannedCoord: Coordonate): Double {
-        val block = scannedCoord.block
-        if (block.hasComparatorInputOverride()) {
-            return block.getComparatorInputOverride(coordonate.world(), coordonate.x, coordonate.y, coordonate.z, forgeSide.ordinal) / 15.0
-        } else if (block.isOpaqueCube) {
-            return 1.0
-        } else if (block.isAir(coordonate.world(), coordonate.x, coordonate.y, coordonate.z)) {
-            return 0.0
-        } else {
-            return 1.0/3.0
-        }
-    }
-
-    private fun scanTileEntity(te: TileEntity): Double {
-        if (te is IFluidHandler) {
-            val info = te.getTankInfo(forgeSide)
-            return info.sumByDouble {
-                (it.fluid?.amount ?: 0).toDouble() / it.capacity
-            } / info.size
-        } else if (te is ISidedInventory) {
-            var sum = 0
-            var limit = 0
-            te.getAccessibleSlotsFromSide(forgeSide.ordinal).forEach {
-                sum += te.getStackInSlot(it)?.stackSize ?: 0
-                limit += te.inventoryStackLimit
-            }
-            return sum.toDouble() / limit
-        } else if (te is IInventory) {
-            val sum = (0..te.sizeInventory - 1).sumBy {
-                te.getStackInSlot(it)?.stackSize ?: 0
-            }
-            return sum.toDouble() / te.inventoryStackLimit / te.sizeInventory
-        } else {
-            return 0.0
-        }
     }
 
     init {
         electricalLoadList.add(output)
         electricalComponentList.add(outputProcess)
         slowProcessList.add(updater)
+    }
+
+    private fun scanBlock(scannedCoord: Coordonate, targetSide: ForgeDirection): Double {
+        val block = scannedCoord.block
+        if (block.hasComparatorInputOverride()) {
+            return block.getComparatorInputOverride(scannedCoord.world(), scannedCoord.x, scannedCoord.y, scannedCoord.z, targetSide.ordinal) / 15.0
+        } else if (block.isOpaqueCube) {
+            return 1.0
+        } else if (block.isAir(scannedCoord.world(), scannedCoord.x, scannedCoord.y, scannedCoord.z)) {
+            return 0.0
+        } else {
+            return 1.0/3.0
+        }
+    }
+
+    private fun scanTileEntity(te: TileEntity, targetSide: ForgeDirection): Double {
+        if (te is IFluidHandler) {
+            val info = te.getTankInfo(targetSide)
+            return info.sumByDouble {
+                (it.fluid?.amount ?: 0).toDouble() / it.capacity
+            } / info.size
+        } else if (te is ISidedInventory) {
+            var sum = 0
+            var limit = 0
+            val slots = te.getAccessibleSlotsFromSide(targetSide.ordinal)
+            when (mode) {
+                ScanMode.SIMPLE -> slots.forEach {
+                        sum += te.getStackInSlot(it)?.stackSize ?: 0
+                        limit += te.inventoryStackLimit
+                    }
+
+                ScanMode.SLOTS -> slots.forEach {
+                    sum += if ((te.getStackInSlot(it)?.stackSize ?: 0) > 0) 1 else 0
+                    limit += 1
+                }
+            }
+            return sum.toDouble() / limit
+        } else if (te is IInventory) {
+            val sum = when (mode) {
+                ScanMode.SIMPLE -> (0..te.sizeInventory - 1).sumBy {
+                    te.getStackInSlot(it)?.stackSize ?: 0
+                }.toDouble()
+
+                ScanMode.SLOTS -> (0..te.sizeInventory - 1).count {
+                    (te.getStackInSlot(it)?.stackSize ?: 0) > 0
+                }.toDouble() * te.inventoryStackLimit
+            }
+            return sum / te.inventoryStackLimit / te.sizeInventory
+        } else {
+            return -1.0
+        }
+    }
+
+    override fun onBlockActivated(entityPlayer: EntityPlayer?, side: Direction?, vx: Float, vy: Float, vz: Float): Boolean {
+        if (super.onBlockActivated(entityPlayer, side, vx, vy, vz)) return true
+        mode = when (mode) {
+            ScanMode.SIMPLE -> ScanMode.SLOTS
+            ScanMode.SLOTS -> ScanMode.SIMPLE
+        }
+        needPublish()
+        return true
     }
 
     override fun getElectricalLoad(lrdu: LRDU?) = output
@@ -102,7 +141,7 @@ class ScannerElement(sixNode: SixNode, side: Direction, descriptor: SixNodeDescr
         else -> 0
     }
 
-    override fun multiMeterString() = Utils.plotPercent("", outputProcess.outputNormalized)
+    override fun multiMeterString() = "Mode: ${tr(mode.name.toLowerCase().capitalize())}, Value: ${Utils.plotPercent("", outputProcess.outputNormalized)}"
 
     override fun thermoMeterString() = null
 
