27d6c902d8cb1bd1d86d8857e02b08210eb8cc55
Anar Abdullayev <anaryek@hotmail.com.tr>
2017-05-05 23:27:43 +0400

    mtsa

--- src/main/java/mods/eln/Eln.java
+++ src/main/java/mods/eln/Eln.java
@@ -25,6 +25,7 @@ import mods.eln.ghost.GhostManager;
 import mods.eln.ghost.GhostManagerNbt;
 import mods.eln.gridnode.electricalpole.ElectricalPoleDescriptor;
 import mods.eln.i18n.I18N;
+import mods.eln.integration.minetweaker.MinetweakerIntegration;
 import mods.eln.item.*;
 import mods.eln.item.electricalinterface.ItemEnergyInventoryProcess;
 import mods.eln.item.electricalitem.*;
@@ -957,6 +958,9 @@ public class Eln {
 
         serverEventListener = new ServerEventListener();
 
+		if(Loader.isModLoaded("MineTweaker3")){
+			MinetweakerIntegration.instance.initialize();
+		}
     }
 
 	/*
