ac08285d8c0b7c4c5165a1239e2b46049d13c1ac
lolmegaxd1 <lolmegaxd1@gmail.com>
2018-08-20 09:02:27 +0000

    Update ElectricalWindSensorElement.java

--- src/main/java/mods/eln/sixnode/electricalwindsensor/ElectricalWindSensorElement.java
+++ src/main/java/mods/eln/sixnode/electricalwindsensor/ElectricalWindSensorElement.java
@@ -85,7 +85,7 @@ public class ElectricalWindSensorElement extends SixNodeElement {
     public void networkSerialize(DataOutputStream stream) {
         super.networkSerialize(stream);
         try {
-            stream.writeFloat((float) Utils.getWind(sixNode.coordonate.dimention, sixNode.coordonate.y));
+            stream.writeFloat((float) Utils.getWind(sixNode.coordonate.getDimension(), sixNode.coordonate.y));
         } catch (IOException e) {
             e.printStackTrace();
         }
