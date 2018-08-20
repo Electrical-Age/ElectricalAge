590190a409834d8ce44ae7c8b37919cbd1fd4b18
lolmegaxd1 <lolmegaxd1@gmail.com>
2018-08-20 08:19:51 +0000

    Update BatteryChargerElement.java

--- src/main/java/mods/eln/sixnode/batterycharger/BatteryChargerElement.java
+++ src/main/java/mods/eln/sixnode/batterycharger/BatteryChargerElement.java
@@ -142,7 +142,7 @@ public class BatteryChargerElement extends SixNodeElement {
         if (onBlockActivatedRotate(entityPlayer)) {
             return true;
         } else {
-            return inventory.take(entityPlayer.getCurrentEquippedItem(), this, false, true);
+            return inventory.take(entityPlayer.getHeldItemMainhand(), this, false, true);
         }
     }
 
