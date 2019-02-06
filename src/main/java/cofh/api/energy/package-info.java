84b3fa7ccf10cac29495a242c6ee3214921a2e1e
Svein Ove Aas <sveina@gmail.com>
2019-02-06 01:02:20 +0000

    Add the 1.10 COFH RF API

---
+++ src/main/java/cofh/api/energy/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * (C) 2014-2017 Team CoFH / CoFH / Cult of the Full Hub
+ * http://www.teamcofh.com
+ */
+@API (apiVersion = CoFHAPIProps.VERSION, owner = "cofhapi", provides = "cofhapi|energy")
+package cofh.api.energy;
+
+import cofh.api.CoFHAPIProps;
+import net.minecraftforge.fml.common.API;
---
+++ src/main/java/cofh/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * (C) 2014-2017 Team CoFH / CoFH / Cult of the Full Hub
+ * http://www.teamcofh.com
+ */
+@API (apiVersion = CoFHAPIProps.VERSION, owner = "cofhlib", provides = "cofhapi")
+package cofh.api;
+
+import net.minecraftforge.fml.common.API;
